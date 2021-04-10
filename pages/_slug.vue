1d0222433ff16ce0f64184acb00e17352e68ead8
Patrick Hurley <pathurley7@gmail.com>
2021-04-10 13:46:07 +0100

    Added axios calls on articles list and a new article page

---
+++ pages/_slug.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <NuxtLink class="button" to="/">Back to home</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      article: '',
+    }
+  },
+  async mounted() {
+    const response = await this.$axios.$get(`articles/${this.$route.query.id}`)
+    this.article = response
+  },
+}
+</script>
+
+<style lang="scss">
+.button {
+  border: 2px solid green;
+  color: green;
+  text-decoration: none;
+  padding: 10px 15px;
+  font-size: 20px;
+  display: inline-block;
+  margin-top: 30px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,58 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">frontend</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <h1>Nuxt, Strapi, Postres, REST</h1>
+    <div v-for="article in articles" :key="article.id">
+      <div class="article">
+        <NuxtLink :to="{ path: article.slug, query: { id: article.id } }">{{
+          article.title
+        }}</NuxtLink>
+        <p>{{ article.subheading }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      articles: [],
+    }
+  },
+  async mounted() {
+    const response = await this.$axios.$get('articles')
+    this.articles = response
+  },
+}
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  max-width: 800px;
 }
 
-.title {
+h1 {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
+  font-size: 50px;
+  text-transform: uppercase;
   letter-spacing: 1px;
+  text-align: center;
+  margin: 30px 0;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.article {
+  padding: 20px 0;
+  border-bottom: 1px solid #d3d3d3;
+  a {
+    display: block;
+    color: green;
+    font-size: 30px;
+    text-decoration: none;
+    margin-bottom: 10px;
+  }
 }
 </style>
